9a960e13d05627a6d68d42893a6427c1d5ebade8
luis fernando <chaoslecion71@gmail.com>
2020-08-23 16:20:05 -0500

    save code

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['casazola-api.ga'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['api-casazola-unac.tk'])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
